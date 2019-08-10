7915c60f1b4f574170ab182fad4ab2893ac23f07
mutenkahoshiume <53928604+mutenkahoshiume@users.noreply.github.com>
2019-08-11 08:54:50 +0900

    jQueryの適用　面倒くさかったマジで

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-
+jQuery(function($){
+	var current;
+	$.scrollify({
+		section:".box",
+		setHeights:false,
+		before:function(i,box){
+			current = i;
+		},
+	});
+	$(window).on('resize',function(){
+		if(current){
+            var currentScrl = $('.box').eq(current).offset().top;
+			$(window).scrollTop(currentScrl);
+        }
+	});
+});
