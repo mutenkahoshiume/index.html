09a368f47e7b99edd802f7fa548f4d37168ffa79
mutenkahoshiume <53928604+mutenkahoshiume@users.noreply.github.com>
2019-08-14 13:26:08 +0900

    urlを付ける

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-jQuery(function($){
-	var current;
-	$.scrollify({
+jQuery(function ($) {
+    var current;
+    $.scrollify({
         scrollSpeed: 1100,
-		section:".box",
-		setHeights:false,
-		before:function(i,box){
-			current = i;
-		},
-	});
-	$(window).on('resize',function(){
-		if(current){
+        section: ".box",
+        setHeights: false,
+        before: function (i, box) {
+            current = i;
+        },
+    });
+    $(window).on('resize', function () {
+        if (current) {
             var currentScrl = $('.box').eq(current).offset().top;
-			$(window).scrollTop(currentScrl);
+            $(window).scrollTop(currentScrl);
         }
-	});
+    });
+});
+
+jQuery('.shikaku').click(function () {
+    location.href = jQuery(this).attr('data-url');
 });
