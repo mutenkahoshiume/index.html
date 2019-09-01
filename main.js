287593e862d83f458cbf5cff46866a18ec094fd3
mutenkahoshiume <53928604+mutenkahoshiume@users.noreply.github.com>
2019-09-01 09:46:31 +0900

    アップデート(Giyhubの項目を追加)

--- main.js
+++ main.js
@@ -16,21 +16,3 @@ jQuery(function ($) {
         }
     });
 });
-
-
-//New Pages Count Down We Are Perfect I Guess That Would Be All Right
-var count = new Date("sept 1,2019 10:00:00").getTime();
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var d = count - now;
-
-    var days = Math.floor(d / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((d % (1000 * 60)) / 1000);
-
-    document.getElementById("day").innerHTML = days;
-    document.getElementById("hour").innerHTML = hours;
-    document.getElementById("min").innerHTML = minutes;
-    document.getElementById("second").innerHTML = seconds;
-});
